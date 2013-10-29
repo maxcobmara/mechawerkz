0307ff6599f71f740d6c9608230df67da779004d
MaxCobMara <maxcobmara@gmail.com>
2013-10-30 01:04:49 +0800

    fixes and notes

--- app/models/document_binder.rb
+++ app/models/document_binder.rb
@@ -1,5 +1,14 @@
 class DocumentBinder < ActiveRecord::Base
-	has_many :staff_position,  :foreign_key => 'position.id', 'position.name'
-	accepts_nested_attributes_for :staff_position, allow_destroy: true
+  
+  #Your code commented out ~ delete comments when done
+	#has_many :staff_position,  :foreign_key => 'position.id', 'position.name'
+  
+  ## Read out loud : Document Binder has many staff positions.  logic?
+  
+	#accepts_nested_attributes_for :staff_position, allow_destroy: true
+  ## this is only required with nested forms i.e add many items in one form. we only have one drop down.
+  
+  belongs_to :staff_position, :foreign_key => 'owned_by'
+  #read out loud: DocumentBinder belongs to Staff Position. logic?
 	
 end
--- app/models/staff_position.rb
+++ app/models/staff_position.rb
@@ -1,5 +1,17 @@
 class StaffPosition < ActiveRecord::Base
   has_ancestry
   
-  belong_to :document_binder,  :foreign_key => 'position.id', 'position.name'
+  #belong_to :document_binder,  :foreign_key => 'position.id', 'position.name'
+  ## e.g Binder = Accounts, Staff Position = Accountant.
+  ## read out loud : Accountant belongs to Accounts Binder. aik! orang bertuankan fail? tak logic
+  ## :foreign_key => 'position.id' means database: position, field: id.  this doesnt exist.
+  
+  has_many :document_binders, foreign_key: 'owned_by'
+  
+  #note plural, so read out loud: Staff Position has many document binders. logic?
+  #now with data.  Accountant has many document binders.
+  #now if singular: Account has many document binder.  grammar?
+  #note there is also has_one
+  #e.g  Car belongs_to owner, owner has_one car. (tho owner may have many cars but allowed only one)
+
   end
