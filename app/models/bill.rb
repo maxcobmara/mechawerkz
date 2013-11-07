3d1f6a351e8cc3a72fd8801e799f70d63feb2618
irshad89 <isyaduddin@gmail.com>
2013-11-07 17:15:24 +0800

    drop down menu

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,2 +1,5 @@
 class Bill < ActiveRecord::Base
+	belongs_to :project, :foreign_key => 'project_id'
+	belongs_to :contact_company, :foreign_key => 'pay_to'
+	belongs_to :document, :foreign_key => 'document_id' 
 end
--- app/models/contact_company.rb
+++ app/models/contact_company.rb
@@ -3,4 +3,5 @@ class ContactCompany < ActiveRecord::Base
   accepts_nested_attributes_for :contact_mediums, allow_destroy: true
   
   has_many :contact_persons
+  has_many :bill, :foreign_key => 'pay_to'
 end
